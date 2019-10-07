c60a15d481a465b0c954a53bac0bb31ed2894e1e
Federico <fdsosa.35@gmail.com>
2019-10-07 17:50:55 -0300

    Added variables for deploy

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ var server = express();
 var path = require('path');
 var port = process.env.PORT || 3000;
 
-var env = process.env.ENV || 'DEV';
-var folderBuild = env == 'PROD' ? '/dist/' : '/.temp/';
-
-var mongoUrl = 'mongodb://localhost:27017/';
-var dbname = 'proyect-cv';
+var mongoUrl = MONGO_URI || 'mongodb://localhost:27017/';
+var dbname = DB_NAME || 'proyect-cv';
 
 server.use(express.static(path.join(__dirname, folderBuild)));
 server.use(cors());
@@ -18,77 +15,43 @@ server.use(cors());
 function getCollection(collectionName) {
     return mongoClient
         .connect(mongoUrl)
-        .then(function (client) {
-            return client.db(dbname);
-        })
-        .then(function (database) {
-            return database.collection(collectionName);
-        })
+        .then(client => client.db(dbname))
+        .then(database => database.collection(collectionName))
 }
 
 server.get('/api/datos-personales', function (req, res) {
     getCollection('personal-data')
-        .then(function (collection) {
-            return collection.find({}).toArray();
-        })
-        .then(function (results) {
-            return res.status(200).send(results[0] || {});
-        })
-        .catch(function (error) {
-            return res.status(500).send(error);
-        });
+        .then( collection => collection.find({}).toArray())
+        .then( results    => res.status(200).send(results[0] || {}))
+        .catch( error     => res.status(500).send(error))
 });
   
 server.get('/api/social-networks', function (req, res) {
     getCollection('social-networks')
-        .then(function (collection) {
-            return collection.find({}).toArray();
-        })
-        .then(function (results) {
-            return res.status(200).send(results);
-        })
-        .catch(function (error) {
-            return res.status(500).send(error);
-        });
+        .then(collection => collection.find({}).toArray())
+        .then(results    => res.status(200).send(results))
+        .catch(error     => res.status(500).send(error))
 });
   
 server.get('/api/description', function (req, res) {
     getCollection('description')
-        .then(function (collection) {
-            return collection.find({}).toArray();
-        })
-        .then(function (results) {
-            return res.status(200).send(results[0] || {});
-        })
-        .catch(function (error) {
-            return res.status(500).send(error);
-        });
+        .then(collection => collection.find({}).toArray())
+        .then(results    => res.status(200).send(results[0] || {}))
+        .catch(error     => res.status(500).send(error))
 });
   
 server.get('/api/skills', function (req, res) {
     getCollection('skills')
-        .then(function (collection) {
-            return collection.find({}).toArray();
-        })
-        .then(function (results) {
-            return res.status(200).send(results);
-        })
-        .catch(function (error) {
-            return res.status(500).send(error);
-        });
+        .then(collection => collection.find({}).toArray())
+        .then(results => res.status(200).send(results))
+        .catch(error => res.status(500).send(error))
 });
   
 server.get('/api/skill', function (req, res) {
     getCollection('skills')
-        .then(function (collection) {
-            return collection.find({name:"CSS"}).toArray();
-        })
-        .then(function (results) {
-            return res.status(200).send(results);
-        })
-        .catch(function (error) {
-            return res.status(500).send(error);
-        });
+        .then(collection => collection.find({name:"CSS"}).toArray())
+        .then(results => res.status(200).send(results))
+        .catch(error => res.status(500).send(error))
 });
 
 server.listen(port, function () {
